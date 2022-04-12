2ebc48c4fa1c72ff943f382ba9e5b8801b996890
wzeng0 <wzeng00@uchicago.edu>
2022-04-12 04:48:16 +0000

    bot successfully follows person

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 # imports scan to get information about environment
@@ -22,13 +22,33 @@ class FollowPerson(object):
         lin = Vector3()
         ang = Vector3()
         self.twist = Twist(linear=lin,angular=ang)
-        self.side_count = 0
+        # self.scan_pub = LaserScan(range_max = .8)
 
     def scan_callback(self, data):
-        if (data.ranges[0] == 0.0 or data.ranges[0] >= distance):
-            self.twist.linear.x = 0.1
-        else:
+        # Finds what angle at which the robot should turn
+        lowest_angle = -1
+        smallest_val = 100
+        for x in range(360):
+            if (data.ranges[x] < smallest_val) and (data.ranges[x] != 0):
+                smallest_val = data.ranges[x]
+                lowest_angle = x
+        if (lowest_angle == -1):
+            self.twist.angular.z = 0
             self.twist.linear.x = 0
+        else:
+            # Rotates left and right according to where an object is found
+            if (lowest_angle >= 180):
+                self.twist.angular.z = (-(360 - lowest_angle) / 180) * 1.82
+            else:
+                self.twist.angular.z = (lowest_angle / 180) * 1.82
+
+            # stop bot if it is the distance away from object
+            if (data.ranges[0] < distance) and (data.ranges[0] != 0):
+                self.twist.linear.x = 0
+            else:
+                self.twist.linear.x = 0.1
+
+        # publishes the movement accordingly
         self.movement_pub.publish(self.twist)
 
     def run(self):
