f38fe95ed529b40feaa364a0a0f05c02839b57c1
wzeng0 <wzeng00@uchicago.edu>
2022-04-13 02:53:06 +0000

    description for robot

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -22,22 +22,24 @@ class FollowPerson(object):
         lin = Vector3()
         ang = Vector3()
         self.twist = Twist(linear=lin,angular=ang)
-        # self.scan_pub = LaserScan(range_max = .8)
 
     def scan_callback(self, data):
         # Finds what angle at which the robot should turn
-        lowest_angle = -1
-        smallest_val = 100
-        for x in range(360):
+        lowest_angle = -1 # this is an initialization that tells me whether there is anything around the robot's environemnt
+        smallest_val = 100 # this just has to be a big enough number so that I can figure out which angle detects an object the closest
+        for x in range(360): # in range of 360 because that is the many angles that we check
             if (data.ranges[x] < smallest_val) and (data.ranges[x] != 0):
-                smallest_val = data.ranges[x]
-                lowest_angle = x
-        if (lowest_angle == -1):
+                smallest_val = data.ranges[x] # gives the distance of the angle that is closest to an object
+                lowest_angle = x # the angle that is closest to the object
+
+        if (lowest_angle == -1): # if there is nothing in the environment, then don't move
             self.twist.angular.z = 0
             self.twist.linear.x = 0
         else:
             # Rotates left and right according to where an object is found
             if (lowest_angle >= 180):
+                # 1.82 is the highest speed the robot can turn
+                # 180 is to keep it in the range 0 and -1 (or 0 and 1) for now
                 self.twist.angular.z = (-(360 - lowest_angle) / 180) * 1.82
             else:
                 self.twist.angular.z = (lowest_angle / 180) * 1.82
@@ -53,6 +55,7 @@ class FollowPerson(object):
 
     def run(self):
         rospy.spin()
+
 if __name__ == '__main__':
     node = FollowPerson()
     node.run() 
